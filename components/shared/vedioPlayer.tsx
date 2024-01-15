bc7c3ab8177f41336cf160e17b0782f448d8433e
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2024-01-15 04:32:41 +0300

    fix hydration error

--- components/shared/vedioPlayer.tsx
+++ components/shared/vedioPlayer.tsx
@@ -1,20 +1,30 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 
 interface VideoPlayerProps {
   url: string;
 }
 const VedioPlayer: React.FC<VideoPlayerProps> = ({ url }) => {
-  
+  const [hasWindow, setHasWindow] = useState(false);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setHasWindow(true);
+    }
+  }, []);
   return (
-    <ReactPlayer
-      url={url}
-      height={'20rem'}
-      width={"100%"}
-      loop={true}
-      playing={true}
-    />
+    <div>
+      {hasWindow && (
+        <ReactPlayer
+          url={url}
+          height={"20rem"}
+          width={"100%"}
+          loop={true}
+          playing={true}
+          controls={false}
+        />
+      )}
+    </div>
   );
 };
 
